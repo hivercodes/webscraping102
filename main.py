2fab1d4e03f6243cc4f048716fb3c1b6bdeb8aad
Lars <lars.eriksson.mail@gmail.com>
2022-03-22 19:20:16 +0100

    html sucks. Hard. But got it done..

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-website_request = requests.get(url="https://www.empireonline.com/movies/features/best-movies-2")
+website_request = requests.get(url="https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 data = website_request.text
 
 soup = BeautifulSoup(data, "html.parser")
 
-article = soup.article.find_all("div", {"class": "article-content"})
+all_movies = []
 
-data = article[0]
+data = soup.find_all(name="h3", class_="title")
 
-print(data)
+for d in data:
+    all_movies.append(d.text)
+
+
+print(all_movies[::-1])
+
+with open("movie.list", "a") as movies:
+    for item in all_movies[::-1]:
+        movies.write(f"{item}\n")
